080ec6c06648179103695ec7891ba77aa025d63c
Will Norris <will@willnorris.com>
2013-02-07 20:04:50 -0800

    wordpress 3.5 drops option for disabling xmlrpc

--- functions.php
+++ functions.php
@@ -240,12 +240,6 @@ function pdx_cleanup_wp() {
   remove_action( 'wp_head', 
     array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
 
-  // remove rpc related links if not necessary
-  if ( !get_option('enable_xmlrpc') ) {
-    remove_action('wp_head', 'wlwmanifest_link');
-    remove_action('wp_head', 'rsd_link');
-  }
-
   // remove adjacent links for pages
   if ( is_page() ) {
     remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
